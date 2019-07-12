c3e0458fb2af573da343049eaca6c2c3ac8681d7
zouxudong <1359694358@qq.com>
2019-07-12 15:17:24 +0800

    modify list

--- learnpython/Condition.py
+++ learnpython/Condition.py
@@ -5,6 +5,7 @@ import time
 import requests
 
 
+
 dt=int(time.time())
 print(str(dt))
 url="http://ip.taobao.com/service/getIpInfo.php?ip=myip&time="+str(dt)
@@ -18,10 +19,24 @@ if response.status_code==200:
 
 
 
-mylist=["php","java","kotlin","cpp","erlang"]
+mylist=["php","where","fuckyou","cpp","erlang"]
 
 for i in range(len(mylist)):
-    pass
+    itemLen=len(mylist[i])
+    if itemLen%2!=0:
+        middle=itemLen//2
+        itemChar=mylist[i][:middle]+mylist[i][middle:middle+1].upper()+mylist[i][middle+1:]
+        mylist[i]=itemChar
+print(mylist)
+mylist=["php","where","fuckyou","cpp","erlang"]
+for item in mylist:
+    itemLen = len(item)
+    if itemLen%2!=0:
+        currentIndex=mylist.index(item)
+        middle=itemLen//2
+        itemChar=item[:middle]+item[middle:middle+1].upper()+item[middle+1:]
+        mylist[currentIndex] = itemChar
+print(mylist)
 
 
 score=int(input("please input score num:"))
