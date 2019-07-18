57dc897fd5155ae55ef66266c3bd36aea0d169b8
zouxudong <1359694358@qq.com>
2019-07-18 22:11:04 +0800

    modify list

--- learnpython/EmailSend.py
+++ learnpython/EmailSend.py
@@ -4,7 +4,7 @@ import smtplib
 def sendEmailText():
     smtpserver="smtp.163.com"
     account="zouxudongatcd@163.com"
-    psw="qvbvjai1989"#input("input account psw:")
+    psw=input("input account psw:")
     reciver="15902127953@163.com"
     content=MIMEText("我是烧饼哥 跟着你写")
     server=smtplib.SMTP(smtpserver,25)
@@ -16,4 +16,26 @@ def sendEmailText():
     server.quit()
     pass
 
-sendEmailText()
+# sendEmailText()
+
+def sendEmailHTML():
+    smtpserver = "smtp.163.com"
+    account = "zouxudongatcd@163.com"
+    psw = input("input account psw:")
+    reciver = "15902127953@163.com"
+    htmlfile = open("./baidu.html", "r+")
+    result = htmlfile.readlines()
+    htmlData = ""
+    for itemStr in result:
+        htmlData = htmlData + itemStr
+    content = MIMEText(htmlData,"HTML")
+    server = smtplib.SMTP(smtpserver, 25)
+    server.login(account, psw)
+    content['Subject'] = Header("我是烧饼哥HTML", 'utf-8')
+    content['From'] = account
+    content['To'] = reciver
+    server.sendmail(account, reciver, content.as_string())
+    server.quit()
+    pass
+
+sendEmailHTML()
