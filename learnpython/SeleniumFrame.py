74edbde5d8407bc4bbf2aa430e88d7d31d2c9b96
zouxudong <1359694358@qq.com>
2019-08-04 22:07:51 +0800

    等待

--- learnpython/SeleniumFrame.py
+++ learnpython/SeleniumFrame.py
@@ -1,6 +1,10 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 mobile_elements={"deviceName":"iPhone X"} #define a dic  ,declare device model
 options=webdriver.ChromeOptions()
@@ -8,13 +12,23 @@ options=webdriver.ChromeOptions()
 options.add_argument("start-maximize")
 options.add_experimental_option("mobileEmulation",mobile_elements)
 driver=webdriver.Chrome()#options=options)
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
 driver.get("http://www.kuaidi100.com")
-driver.find_element_by_id("uDeskTarget").click()
-udesk_iframe=driver.find_element_by_id("udesk_iframe")
+
+
+uDeskTarget=WebDriverWait(driver,10,0.5).until(lambda dri:dri.find_element(By.ID,"uDeskTarget"))
+uDeskTarget.click()
+# uDeskTarget=WebDriverWait(driver,10,0.5).until(EC.visibility_of_element_located((By.ID,"uDeskTarget")))
+# uDeskTarget.click()
+
+# driver.find_element_by_id("uDeskTarget").click()
+# udesk_iframe=driver.find_element_by_id("udesk_iframe")
 driver.switch_to.frame("udesk_iframe")
-driver.find_element_by_xpath("//textarea[@rows='1']").send_keys("你好")
-time.sleep(3)
-driver.find_element_by_id("btnSend").click()
+input=WebDriverWait(driver,10,0.5).until(EC.visibility_of_element_located((By.XPATH,"//textarea[@rows='1']")))
+# driver.find_element_by_xpath("//textarea[@rows='1']").send_keys("你好")
+input.send_keys("hello world")
+# time.sleep(3)
+# driver.find_element_by_id("btnSend").click()
+driver.switch_to.default_content()
 time.sleep(8)
 driver.quit()
